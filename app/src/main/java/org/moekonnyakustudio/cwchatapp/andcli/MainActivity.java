5e0b4f700098aca0220f6881c2281fb26a96d95f
chihuo2104 <chihuoqwefengmi13@qq.com>
2020-08-18 06:13:40 -0400

    Can send message.
    But can not resolve message.

--- app/src/main/java/org/moekonnyakustudio/cwchatapp/andcli/MainActivity.java
+++ app/src/main/java/org/moekonnyakustudio/cwchatapp/andcli/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Looper;
+import android.util.Base64;
 import android.util.Log;
 import android.view.*;
 import android.widget.*;
@@ -21,23 +22,27 @@ public class MainActivity extends AppCompatActivity {
     public void ClickOn(View v) {
         EditText iws = findViewById(R.id.wsServerInsert);
         URI uri = URI.create(iws.getText().toString());
-        WebSocketClient wsc=new WebSocketClient(uri) {
+
+        final WebSocketClient wsc=new WebSocketClient(uri) {
             @Override
             public void onOpen(ServerHandshake handshakeData) {
                 Looper.prepare();
                 Toast.makeText(MainActivity.this,"恭喜！服务器连接成功！",Toast.LENGTH_LONG).show();
                 Looper.loop();
                 String HttpStatus=String.valueOf(handshakeData.getHttpStatus());
-                Log.i("WSConnect", HttpStatus);
+                Log.i("WSConnect", "onOpen!");
             }
             @Override
             public void onMessage(String message) {
-
+                TextView showMessage=findViewById(R.id.textView);
+                showMessage.setText("收到信息："+message);
+                Log.i("WSConnect", "onMessage!");
             }
             @Override
             public void onClose(int code, String reason, boolean remote) {
                 Looper.prepare();
                 Toast.makeText(MainActivity.this,"草，服务器关闭了！",Toast.LENGTH_LONG).show();
+                Log.i("WSConnect", "onClose!");
                 Looper.loop();
             }
             @Override
@@ -45,8 +50,20 @@ public class MainActivity extends AppCompatActivity {
                 Looper.prepare();
                 Toast.makeText(MainActivity.this,"呜呜呜！连接失败了！QAQ！",Toast.LENGTH_LONG).show();
                 Looper.loop();
+                Log.i("WSConnect", "onError!");
             }
         };
         wsc.connect();
+        Button sendbut=findViewById(R.id.CommitMessage);
+        sendbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText insText=findViewById(R.id.wsServerMessage);
+                String SM=insText.getText().toString();
+                String encodeStr = Base64.encodeToString(SM.getBytes(), Base64.DEFAULT);
+                wsc.send(String.valueOf(encodeStr));
+                Toast.makeText(MainActivity.this,"恭喜！信息发送成功！",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
