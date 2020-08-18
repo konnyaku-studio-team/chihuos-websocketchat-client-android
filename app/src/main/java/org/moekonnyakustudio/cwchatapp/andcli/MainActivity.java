532609ae67f279b97ba711197676b16b428c56c8
chihuo2104 <chihuoqwefengmi13@qq.com>
2020-08-18 04:34:18 -0400

    Added WS package&&Renew GUI
    Added WS package(java-websocket)
    Renew GUI

--- app/src/main/java/org/moekonnyakustudio/cwchatapp/andcli/MainActivity.java
+++ app/src/main/java/org/moekonnyakustudio/cwchatapp/andcli/MainActivity.java
@@ -2,7 +2,14 @@ package org.moekonnyakustudio.cwchatapp.andcli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
+import android.util.Log;
+import android.view.*;
+import android.widget.*;
 import android.os.Bundle;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void ClickOn(View v) {
+        EditText iws = findViewById(R.id.wsServerInsert);
+        Toast toast = Toast.makeText(MainActivity.this,"QAQ！连接失败了！",Toast.LENGTH_LONG);
+        URI uri = URI.create(String.valueOf(iws.getText()));
+        WebSocketClient wsc=new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake handshakeData) {
+                Toast toast = Toast.makeText(MainActivity.this,"连接成功！",Toast.LENGTH_LONG);
+
+            }
+
+            @Override
+            public void onMessage(String message) {
+
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+            }
+
+            @Override
+            public void onError(Exception ex) {
+                Toast toast = Toast.makeText(MainActivity.this,"QAQ！连接失败了！",Toast.LENGTH_LONG);
+            }
+        };
     }
 }
